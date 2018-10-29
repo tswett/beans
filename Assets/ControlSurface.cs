4f79ec58357cc98260d96b6575795a97c7380af7
Tanner Swett <tannerswett@gmail.com>
2018-10-28 21:51:20 -0400

    Implement control input

---
+++ Assets/ControlSurface.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlSurface : MonoBehaviour
+{
+    public string AxisName;
+    public float Coefficient;
+    HingeJoint hingeJoint;
+
+    void Start()
+    {
+        hingeJoint = GetComponent<HingeJoint>();
+    }
+
+    void Update()
+    {
+        float controlInput = Input.GetAxis(AxisName);
+
+        if (AxisName == "Rudder")
+            Debug.Log(string.Format("Axis {0}, input {1}", AxisName, controlInput));
+
+        JointSpring hingeSpring = hingeJoint.spring;
+        hingeSpring.targetPosition = Coefficient * controlInput;
+        hingeJoint.spring = hingeSpring;
+    }
+}
